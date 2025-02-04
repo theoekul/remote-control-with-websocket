273754ebfda80164c58b7c7bd7f2b0189ffca390
theoekul <theoviktil@gmail.com>
2025-02-04 09:45:49 +0100

    cvbfbdfgdfhfgbfgnfggnfgnfgnnvbnvbbnvbnvb

--- src/rotationread.cpp
+++ src/rotationread.cpp
@@ -11,3 +11,28 @@ void initRotRead() {
     pinMode(ROTR_07,  INPUT);
     pinMode(ROTR_08,  INPUT);
 }
+
+void initRotarySwitch() { // denne funksjonen er for å lese av rotary switchen og skrive ut posisjonen til serial monitor
+    int position = 0;
+    if (digitalRead(ROTR_01) == HIGH) position = 1;
+    else if (digitalRead(ROTR_02) == HIGH) position = 2;
+    else if (digitalRead(ROTR_03) == HIGH) position = 3;
+    else if (digitalRead(ROTR_04) == HIGH) position = 4;
+    else if (digitalRead(ROTR_05) == HIGH) position = 5;
+    else if (digitalRead(ROTR_06) == HIGH) position = 6;
+    else if (digitalRead(ROTR_07) == HIGH) position = 7;
+    else if (digitalRead(ROTR_08) == HIGH) position = 8;
+
+    Serial.print("Rotary switch position: ");
+    Serial.println(position);
+}
+
+void setup() {
+    Serial.begin(9600);
+    initRotarySwitch();
+}
+
+void loop() {
+    initRotarySwitch();
+    delay(500); // Adjust the delay as needed
+}
