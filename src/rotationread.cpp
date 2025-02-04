37674d83b07c67f15db4c7a888758ca558e439ed
sonblims <sonblims@gmail.com>
2025-02-04 10:30:56 +0100

    NO

--- src/rotationread.cpp
+++ src/rotationread.cpp
@@ -11,8 +11,8 @@ void initRotRead() {
     pinMode(ROTR_07,  INPUT);
     pinMode(ROTR_08,  INPUT);
 }
-
-void readRotarySwitch() { // denne funksjonen er for å lese av rotary switchen og skrive ut posisjonen til serial monitor
+// denne funksjonen er for å lese av rotary switchen og skrive ut posisjonen til serial monitor
+void readRotarySwitch() { 
     int position = 0;
     if (digitalRead(ROTR_01) == HIGH) position = 1;
     else if (digitalRead(ROTR_02) == HIGH) position = 2;
@@ -22,17 +22,10 @@ void readRotarySwitch() { // denne funksjonen er for å lese av rotary switchen 
     else if (digitalRead(ROTR_06) == HIGH) position = 6;
     else if (digitalRead(ROTR_07) == HIGH) position = 7;
     else if (digitalRead(ROTR_08) == HIGH) position = 8;
-    else if (digitalRead(ROTR_01) == LOW && digitalRead(ROTR_02) == LOW && digitalRead(ROTR_03) == LOW && digitalRead(ROTR_04) == LOW && digitalRead(ROTR_05) == LOW && digitalRead(ROTR_06) == LOW && digitalRead(ROTR_07) == LOW && digitalRead(ROTR_08) == LOW) {
-        Serial.println("Rotary switch position: Unknown");
-        return;
-    }
-    
+
     Serial.print("Rotary switch position: ");
     Serial.println(position);
-    
-    }
-
-
+}
 
 void setup() {
     Serial.begin(9600);
