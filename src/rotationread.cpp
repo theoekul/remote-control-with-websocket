c7ae4639e8cc136cccf0c58fa6d08e3c070221e7
theoekul <theoviktil@gmail.com>
2025-02-04 08:22:16 +0100

    Update rotationread.cpp

--- src/rotationread.cpp
+++ src/rotationread.cpp
@@ -2,12 +2,12 @@
 #include "RotationRead.h"
 
 void initRotRead() {
-    pinMode(ROTR_01,      INPUT);
-    pinMode(ROTR_02,      INPUT);
-    pinMode(ROTR_03,      INPUT);
-    pinMode(ROTR_04,      INPUT);
-    pinMode(ROTR_05,      INPUT);
-    pinMode(ROTR_06,      INPUT);
-    pinMode(ROTR_07,      INPUT);
-    pinMode(ROTR_08,      INPUT);
+    pinMode(ROTR_01,  INPUT);
+    pinMode(ROTR_02,  INPUT);
+    pinMode(ROTR_03,  INPUT);
+    pinMode(ROTR_04,  INPUT);
+    pinMode(ROTR_05,  INPUT);
+    pinMode(ROTR_06,  INPUT);
+    pinMode(ROTR_07,  INPUT);
+    pinMode(ROTR_08,  INPUT);
 }
