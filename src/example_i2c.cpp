44ff0cec1d26d6bb5f5b6cb6b8a731dea60f42ee
Sabesto <sabesto@gmail.com>
2025-02-12 13:05:18 +0100

    Create example_i2c.cpp

---
+++ src/example_i2c.cpp
@@ -0,0 +1,35 @@
+void setup() {
+     ....
+     Serial.printf("Init I2C");
+     Wire.begin(21, 47);
+     Wire.beginTransmission(0x74);
+     Wire.write(0x06);  // CONFIG Port 0
+     Wire.write(0x00);  // Set all OUTPUT
+     Wire.endTransmission();
+     Wire.beginTransmission(0x74);
+     Wire.write(0x07);  // CONFIG Port 1
+     Wire.write(0x00);  // Set all OUTPUT
+     Wire.endTransmission();
+     Serial.printf("Init I2C DONE!");
+     ...
+ }
+
+
+void loop() {
+...
+if (button.pressed()) {
+     ...
+     Wire.beginTransmission(0x74);
+     Wire.write(0x03);  // Output Port 1
+     Wire.write(0xFF);  // Set all LOW
+     Wire.endTransmission();
+     delay(1000);
+
+     Wire.beginTransmission(0x74);
+     Wire.write(0x03);  // Output Port 1
+     Wire.write(0x00);  // Set all LOW
+     Wire.endTransmission();
+     delay(1000);
+     ...
+     }
+}
